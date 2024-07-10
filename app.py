07423da35c7783936647113d57211bc87ae7ab5c
shubham srivastava <shubhamsrivastava12568@gmail.com>
2024-07-10 21:25:21 +0530

    final commit

--- app.py
+++ app.py
@@ -22,5 +22,22 @@ def predict_api():
 
     return jsonify(output[0]) 
 
-if __name__=="__main__":
+@app.route("/predict",methods=["POST"])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    print(data)
+    passed_value=np.array(data).reshape(1,-1)
+    output=model.predict(passed_value)
+    print(output[0])
+
+    if(output[0]=="Iris-virginica"):
+        predict_img="virginica.png"
+    elif(output[0]=="Iris-versicolor"):
+        predict_img="versicolor.png"
+    elif(output[0]=="Iris-setosa"):
+        predict_img="setosa.png"
+
+    return render_template("Home.html",predict_text=f"The Predicted Species is: {output[0]}",img=predict_img)
+
+if __name__=="__main__": 
     app.run(debug=True)
